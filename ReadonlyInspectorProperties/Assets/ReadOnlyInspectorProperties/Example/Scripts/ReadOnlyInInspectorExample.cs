994234a5583df6b126910f693885b957eda74c6e
malabarMCB <grinevich97@gmail.com>
2017-11-05 17:39:34 +0200

    Add ReadOnlyInInspector attribute, add example

---
+++ ReadonlyInspectorProperties/Assets/ReadOnlyInspectorProperties/Example/Scripts/ReadOnlyInInspectorExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadOnlyInInspectorExample : MonoBehaviour
+{
+    [SerializeField, ReadOnlyInInspector] private int intType = 1;
+    [SerializeField, ReadOnlyInInspector] private float floatType = 2.99f;
+    [SerializeField, ReadOnlyInInspector] private string stringType = "hello";
+    [SerializeField, ReadOnlyInInspector] private bool boolType = true;
+
+    [SerializeField, ReadOnlyInInspector] private ColorSpace enumType = ColorSpace.Gamma;
+
+    [SerializeField, ReadOnlyInInspector] private Vector2 vector2Type = Vector2.one;
+    [SerializeField, ReadOnlyInInspector] private Vector3 vector3Type = Vector3.one;
+    [SerializeField, ReadOnlyInInspector] private Vector4 vector4Type = Vector4.one;
+
+    [SerializeField, ReadOnlyInInspector] private List<int> listType = new List<int> {1, 2, 3};
+}
---
+++ ReadonlyInspectorProperties/Assets/ReadOnlyInspectorProperties/Scripts/Editor/ReadOnlyInInspectorDrawer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyInInspectorAttribute))]
+public class ReadOnlyInInspectorDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+    {
+        string valueStr;
+
+        switch (prop.propertyType)
+        {
+            case SerializedPropertyType.Integer:
+                valueStr = prop.intValue.ToString();
+                break;
+
+            case SerializedPropertyType.Boolean:
+                valueStr = prop.boolValue.ToString();
+                break;
+
+            case SerializedPropertyType.Float:
+                valueStr = prop.floatValue.ToString("0.00000");
+                break;
+
+            case SerializedPropertyType.String:
+                valueStr = prop.stringValue;
+                break;
+
+            case SerializedPropertyType.Enum:
+                valueStr = prop.enumNames[prop.enumValueIndex];
+                break;
+
+            case SerializedPropertyType.Vector2:
+                valueStr = prop.vector2Value.ToString();
+                break;
+
+            case SerializedPropertyType.Vector3:
+                valueStr = prop.vector3Value.ToString();
+                break;
+
+            case SerializedPropertyType.Vector4:
+                valueStr = prop.vector4Value.ToString();
+                break;
+
+            default:
+                valueStr = "(not supported)";
+                break;
+        }
+
+        EditorGUI.LabelField(position, label.text, valueStr);
+    }
+}
